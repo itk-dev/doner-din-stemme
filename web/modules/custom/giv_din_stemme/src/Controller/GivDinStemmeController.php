e0d57621ff98842bf531503068f160aeb7a42fd2
jekuaitk <jekua@aarhus.dk>
2025-03-03 22:25:04 +0100

    Added accent user field and removed postcode and birthcountry

--- web/modules/custom/giv_din_stemme/src/Controller/GivDinStemmeController.php
+++ web/modules/custom/giv_din_stemme/src/Controller/GivDinStemmeController.php
@@ -231,11 +231,10 @@ class GivDinStemmeController extends ControllerBase {
       // Collect metadata.
       $partTextToRead = $part->getValue()['value'];
       $textId = $text->id();
+      $accent = $user->get('field_accent')->value;
       $birthYear = $user->get('field_birth_year')->value;
       $dialect = $user->get('field_dialects')->value;
       $gender = $user->get('field_gender')->value;
-      $postalCode = $user->get('field_postal_code')->value;
-      $countryBirth = $user->get('field_country_birth')->value;
 
       $entity->set('metadata', json_encode([
         'text' => $partTextToRead,
@@ -245,8 +244,7 @@ class GivDinStemmeController extends ControllerBase {
         'birth_year' => $birthYear,
         'dialect' => $dialect,
         'gender' => $gender,
-        'postal_code' => $postalCode,
-        'country_birth' => $countryBirth,
+        'accent' => $accent,
       ]));
 
       $entity->save();
